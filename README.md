7b17e05e16b16924c708cbbc2f3a773c97c27c6c
Joao Pedro Portela <porthunt@Joaos-MacBook-Air.local>
2021-06-03 12:09:16 +0200

    last changes

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # CovidJabNL
 
 1. Install the requirements.
-2. Execute `python covid.py --year <year> --email <email>`
+2. Execute `./covid.py --year <year> --dest-email <email> --source-email <email>`
 
-When the year that you insert is getting called for vaccination, the email that you input will receive an email.
+When the year that you insert is getting called for vaccination, the email that you input will receive an email. AWS SES needs validated emails, so the source email should also be validated. If your account is not on sandbox mode, sending to whatever email works.
 
 ### Deploy
 
--- covid.py
+++ covid.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python3
 import json
 import argparse
 import boto3
 import requests
 
 
-def send_email(email):
+def send_email(dest, source, year):
     client = boto3.client('ses',region_name="eu-west-1")
     client.send_email(
         Destination={
-            'ToAddresses': [email],
+            'ToAddresses': [dest],
         },
         Message={
             'Body': {
@@ -19,18 +20,19 @@ def send_email(email):
             },
             'Subject': {
                 'Charset': "UTF-8",
-                'Data': "Covid jab",
+                'Data': f"{year} Covid jab",
             },
         },
-        Source="Covid <co@vid.com>",
+        Source=source,
     )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Covid")
     parser.add_argument("--year", required=True, type=str)
-    parser.add_argument("--email", required=True, type=str)
+    parser.add_argument("--dest-email", required=True, type=str)
+    parser.add_argument("--source-email", required=True, type=str)
     args = parser.parse_args()
 
     response = requests.get(f"https://user-api.coronatest.nl/vaccinatie/programma/bepaalbaar/{args.year}/NEE/NEE")
     if json.loads(response.content).get("success"):
-        send_email(args.email)
+        send_email(args.dest_email, args.source_email, args.year)
